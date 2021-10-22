bc6c9f3521612b1f9082a91b771b77bf3fb1cf0e
beterzi81 <beterzi81@gmail.com>
2021-10-22 20:20:37 +0300

    week 1-2

---
+++ AlgoritmaAnaliziLab02/Lab02Soru01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab02Soru01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -5;
+
+            float sonuc = Power(a, -2);
+            Console.WriteLine(a + " sayısının istediğiniz sonucu:  " + sonuc);
+        }
+
+        private static float Power(int a, int v)
+        {
+            float sonuc = a;
+            float ilkA = a;
+            if (v == 0)
+            {
+                return 1;
+            }
+            else if (v < 0)
+            {
+                int vMutlak = -v;
+                ilkA = 1 / ilkA;
+                int i = 0;
+
+                do
+                {
+                    sonuc = ilkA * sonuc;
+                    i++;
+                } while (i <= vMutlak);
+
+                return sonuc;
+            }
+            else
+            {
+                for (int i = 1; i < v; i++)
+                {
+                    sonuc = ilkA * sonuc;
+                }
+                return sonuc;
+            }
+        }
+    }
+}
---
+++ AlgoritmaAnaliziLab02/Lab02Soru02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lab02Soru02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[] { 65, 22, 73, 78, 24, 3, 77, 23, 84, 42 };//Soru 2 burada yapıldı.
+            //Soru 3 burada başlıyor.
+            int temp = dizi[0];
+            dizi[0] = dizi[dizi.Length - 1];
+            dizi[dizi.Length - 1] = temp;
+            //Soru 3 burada bitiyor.
+
+
+            Swap(dizi, 0, 5);//4. sorunun fonksiyonu
+
+
+            BubbleSort(dizi);//5. sorunun fonksiyonu
+            foreach (var item in dizi)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+
+        private static void BubbleSort(int[] dizi)//5. sorunun fonksiyonu
+        {
+            int controllerDegisken = 0;
+
+            while (controllerDegisken != dizi.Length - 1)
+            {
+                controllerDegisken = 0;
+
+                for (int i = dizi.Length - 1; i > 0; i--)
+                {
+
+                    if (dizi[i] < dizi[i - 1])
+                    {
+                        Swap(dizi, i, i - 1);
+
+                    }
+                    else
+                    {
+                        controllerDegisken += 1;
+                    }
+                }
+            }
+
+        }
+
+        private static void Swap(int[] dizi, int v1, int v2)//4. sorunun fonksiyonu
+        {
+            int temporary = dizi[v1];
+            dizi[v1] = dizi[v2];
+            dizi[v2] = temporary;
+        }
+    }
+}
